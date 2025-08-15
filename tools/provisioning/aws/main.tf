9491cdf4221d406d1cb6b7a10332ffb1348904f4
Demo User <contact.spearai@gmail.com>
2025-08-16 00:01:21 +0530

    Automated remediation of security findings

--- tools/provisioning/aws/main.tf
+++ tools/provisioning/aws/main.tf
@@ -108,13 +108,15 @@ resource "aws_instance" "tf_test_vm" {
 
   key_name = "${var.aws_public_key_name}"
 
-  security_groups = [
-    "${aws_security_group.allow_ssh.name}",
-    "${aws_security_group.allow_docker.name}",
-    "${aws_security_group.allow_weave.name}",
-    "${aws_security_group.allow_private_ingress.name}",
-    "${aws_security_group.allow_all_egress.name}",
+  vpc_security_group_ids = [
+    "${aws_security_group.allow_ssh.id}",
+    "${aws_security_group.allow_docker.id}",
+    "${aws_security_group.allow_weave.id}",
+    "${aws_security_group.allow_private_ingress.id}",
+    "${aws_security_group.allow_all_egress.id}",
   ]
+
+  subnet_id = "${var.subnet_id}"
 
   # Wait for machine to be SSH-able:
   provisioner "remote-exec" {
